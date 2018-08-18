2bcd49642e3b19461d2690642e75fa1ecc752544
Qinghai Fu <qifu@paypal.com>
2018-08-18 15:16:08 -0700

    add enable config

--- smssenderservice/src/main/java/com/paypal/ho/impl/SMSService.java
+++ smssenderservice/src/main/java/com/paypal/ho/impl/SMSService.java
@@ -1,6 +1,7 @@
 package com.paypal.ho.impl;
 
 import com.paypal.ho.sms.SmsSender;
+import com.twilio.rest.api.v2010.account.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +14,18 @@ public class SMSService {
     @Autowired
     private SmsSender smsSender;
 
+    private boolean enabled = true;
+
     @RequestMapping("/send-sms")
     public SMSResponse sendSMS(@RequestBody SMSRequest content) {
         System.out.println("sms content " + content);
         SMSResponse response = new SMSResponse();
         response.setStatus("success");
-        smsSender.sendSMS(content);
+        if (enabled) {
+            final Message.Status status = smsSender.sendSMS(content);
+            response.setStatus(status.name());
+
+        }
         return response;
     }
 }
